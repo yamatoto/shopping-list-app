7cb30c7835ac97dd708916495ab6bad8a0eb43f6
yamatoto <mftb35ymt@yahoo.co.jp>
2024-09-05 00:09:29 +0900

    メモ画面レイアウト調整

--- components/MemoModal.tsx
+++ components/MemoModal.tsx
@@ -22,6 +22,8 @@ const MemoModal: React.FC<MemoModalProps> = ({ visible, onClose }) => {
     const { noteMap, fetchNote, updateNote } = useNote();
     const [yamatoText, setYamatoText] = useState('');
     const [mihoText, setMihoText] = useState('');
+    const [yamatoHeight, setYamatoHeight] = useState(100); // 初期高さ
+    const [mihoHeight, setMihoHeight] = useState(100); // 初期高さ
 
     useEffect(() => {
         if (visible) {
@@ -60,12 +62,18 @@ const MemoModal: React.FC<MemoModalProps> = ({ visible, onClose }) => {
                 <View style={styles.inputContainer}>
                     <Text style={styles.label}>Yamatoのメモ</Text>
                     <TextInput
-                        style={styles.textArea}
+                        style={[styles.textArea, { height: yamatoHeight }]} // 高さをstateで管理
                         multiline
                         numberOfLines={4}
                         value={yamatoText}
                         onChangeText={setYamatoText}
+                        onContentSizeChange={event =>
+                            setYamatoHeight(
+                                event.nativeEvent.contentSize.height,
+                            )
+                        } // コンテンツの高さに応じて変更
                         editable={currentUser?.name === 'yamato'}
+                        placeholderTextColor="#000"
                     />
                     {currentUser?.name === 'yamato' && (
                         <Button title="更新" onPress={handleYamatoSubmit} />
@@ -75,12 +83,16 @@ const MemoModal: React.FC<MemoModalProps> = ({ visible, onClose }) => {
                 <View style={styles.inputContainer}>
                     <Text style={styles.label}>Mihoのメモ</Text>
                     <TextInput
-                        style={styles.textArea}
+                        style={[styles.textArea, { height: mihoHeight }]} // 高さをstateで管理
                         multiline
                         numberOfLines={4}
                         value={mihoText}
                         onChangeText={setMihoText}
+                        onContentSizeChange={event =>
+                            setMihoHeight(event.nativeEvent.contentSize.height)
+                        } // コンテンツの高さに応じて変更
                         editable={currentUser?.name === 'miho'}
+                        placeholderTextColor="#000"
                     />
                     {currentUser?.name === 'miho' && (
                         <Button title="更新" onPress={handleMihoSubmit} />
@@ -123,9 +135,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 5,
         padding: 10,
-        height: 100,
         textAlignVertical: 'top',
         marginBottom: 10,
+        color: '#000', // 常に黒色に設定
     },
 });
 
