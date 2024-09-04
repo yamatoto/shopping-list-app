91dd27e8fe6e55702a3d9c636786cefa3789fab1
yamatoto <mftb35ymt@yahoo.co.jp>
2024-09-05 00:05:21 +0900

    設定画面レウィアウト調整

--- app/(tabs)/configure.tsx
+++ app/(tabs)/configure.tsx
@@ -11,8 +11,11 @@ import { sharedStyles } from '@/styles/sharedStyles';
 import SignOut from '@/components/SignOut';
 import PlannedFeaturesModal from '@/components/PlannedFeaturesModal';
 import MemoModal from '@/components/MemoModal';
+import useFirebaseAuth from '@/hooks/useFirebaseAuth';
 
 export default function ConfigureScreen() {
+    const { currentUser } = useFirebaseAuth();
+
     const [plannedFeaturesModalVisible, setPlannedFeaturesModalVisible] =
         useState(false);
     const [memoModalVisible, setMemoModalVisible] = useState(false);
@@ -26,19 +29,27 @@ export default function ConfigureScreen() {
 
     return (
         <View style={sharedStyles.container}>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={handleTogglePlannedFeaturesModal}
-            >
-                <Text style={styles.buttonText}>実装予定</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={handleToggleMemoModal}
-            >
-                <Text style={styles.buttonText}>メモ</Text>
-            </TouchableOpacity>
-            <SignOut />
+            <View style={styles.topButtonsContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleTogglePlannedFeaturesModal}
+                >
+                    <Text style={styles.buttonText}>実装予定</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleToggleMemoModal}
+                >
+                    <Text style={styles.buttonText}>メモ</Text>
+                </TouchableOpacity>
+            </View>
+
+            {currentUser?.name === 'yamato' && (
+                <View style={styles.signOutContainer}>
+                    <SignOut />
+                </View>
+            )}
+
             <PlannedFeaturesModal
                 visible={plannedFeaturesModalVisible}
                 onClose={handleTogglePlannedFeaturesModal}
@@ -52,6 +63,15 @@ export default function ConfigureScreen() {
 }
 
 const styles = StyleSheet.create({
+    topButtonsContainer: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    signOutContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
     button: {
         backgroundColor: Platform.OS === 'ios' ? '#007AFF' : '#2196F3',
         paddingVertical: 10,
@@ -65,6 +85,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.8,
         shadowRadius: 2,
         elevation: 5,
+        width: '100%',
     },
     buttonText: {
         color: '#fff',
