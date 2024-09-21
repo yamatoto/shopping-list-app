1e28e0beb48ed7e7b0144618e155c1b8e6e90114
yamatoto <mftb35ymt@yahoo.co.jp>
2024-09-21 19:10:15 +0900

    Toast短く下に表示

--- shared/helpers/toast.ts
+++ shared/helpers/toast.ts
@@ -2,7 +2,7 @@ import Toast from 'react-native-root-toast';
 
 export const showToast = (message: string) => {
     Toast.show(message, {
-        duration: Toast.durations.LONG,
-        position: Toast.positions.CENTER,
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
     });
 };
